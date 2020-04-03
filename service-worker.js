519ef00ce67dd90f7a43c9c941ac6a1501927dc0
Emerson Walsh <emersonwalsh@Emersons-MacBook-Pro.local>
2020-04-03 15:17:24 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/best-of-tech/precache-manifest.9672770a2730d90816bb5a0b7706dad4.js"
+  "/best-of-tech/precache-manifest.5eb3ad5a95c569fe7dcaadcee2a3651c.js"
 );
 
 self.addEventListener('message', (event) => {
