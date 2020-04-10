a345c7d0c04c42c2a8aa3a42a0a4948eafc239dc
Emerson Walsh <emersonwalsh@Emersons-MacBook-Pro.local>
2020-04-09 23:50:24 -0400

    Handling database format change

--- src/components/Chart.js
+++ src/components/Chart.js
@@ -5,58 +5,59 @@ import Title from './Title';
 
 // Generate Sales Data
 function createData(time, percentage) {
-  return { time, percentage };
+	return { time, percentage };
 }
 
 const formatData = (portfolioHistory) => {
-  let historicalData = [];
-  for (let i = portfolioHistory.length - 1; i >= 0 ; i--) {
-    historicalData.push(createData(
-      portfolioHistory[i].Date,
-      Number(portfolioHistory[i]['Total Percent Change'])
-    ));
-    
-  }
-  return historicalData;
+	let historicalData = [];
+	for (let i = portfolioHistory.length - 1; i >= 0; i--) {
+		let percentChangeStr = portfolioHistory[i]['Total Percent Change']
+		historicalData.push(createData(
+			portfolioHistory[i].Date,
+			Number(percentChangeStr.substring(0, percentChangeStr.length - 1))
+		));
+
+	}
+	return historicalData;
 }
 
 export default function Chart(props) {
-  const theme = useTheme();
+	const theme = useTheme();
 
-  const data = formatData(props.portfolioHistory);
+	const data = formatData(props.portfolioHistory);
 
-  return (
-    <React.Fragment>
-      <Title>Value Over Time</Title>
-      <ResponsiveContainer>
-        <LineChart
-          data={data}
-          margin={{
-            top: 16,
-            right: 16,
-            bottom: 0,
-            left: 24,
-          }}
-        >
-          <XAxis dataKey="time" stroke={theme.palette.text.secondary} />
-          <YAxis 
-            stroke={theme.palette.text.secondary}
-            // axisLine={false}
-            // tick={false}
-            domain={['auto', 'auto']}
-          >
-            <Label
-              angle={270}
-              position="left"
-              style={{ textAnchor: 'middle', fill: theme.palette.text.primary }}
-            >
-              % Change
-            </Label>
-          </YAxis>
-          <Tooltip />
-          <Line type="monotone" dataKey="percentage" stroke={theme.palette.primary.main} dot={false} />
-        </LineChart>
-      </ResponsiveContainer>
-    </React.Fragment>
-  );
+	return (
+		<React.Fragment>
+			<Title>Value Over Time</Title>
+			<ResponsiveContainer>
+				<LineChart
+					data={data}
+					margin={{
+						top: 16,
+						right: 16,
+						bottom: 0,
+						left: 24,
+					}}
+				>
+					<XAxis dataKey="time" stroke={theme.palette.text.secondary} />
+					<YAxis
+						stroke={theme.palette.text.secondary}
+						// axisLine={false}
+						// tick={false}
+						domain={['auto', 'auto']}
+					>
+						<Label
+							angle={270}
+							position="left"
+							style={{ textAnchor: 'middle', fill: theme.palette.text.primary }}
+						>
+							% Change
+						</Label>
+					</YAxis>
+					<Tooltip />
+					<Line type="monotone" dataKey="percentage" stroke={theme.palette.primary.main} dot={false} />
+				</LineChart>
+			</ResponsiveContainer>
+		</React.Fragment>
+	);
 }
--- src/components/Summary.js
+++ src/components/Summary.js
@@ -4,67 +4,67 @@ import Typography from '@material-ui/core/Typography';
 import Title from './Title';
 
 const useStyles = makeStyles({
-  neutral: {
-    flex: 1,
-  },
-  positive: {
-    flex: 1,
-    color: 'green'
-  },
-  negative: {
-    flex: 1,
-    color: 'red'
-  }
+	neutral: {
+		flex: 1,
+	},
+	positive: {
+		flex: 1,
+		color: 'green'
+	},
+	negative: {
+		flex: 1,
+		color: 'red'
+	}
 });
 
 const formatPercentage = (currentValue) => {
-  if (!currentValue) {
-    return '';
-  }
-  let formattedValue = Number(currentValue);
-  if (formattedValue > 0) {
-    return '+' + formattedValue.toFixed(2) + '%';
-  }
-  return formattedValue.toFixed(2) + '%';
+	if (!currentValue) {
+		return '';
+	}
+	let formattedValue = Number(currentValue.substring(0, currentValue.length - 1));
+	if (formattedValue > 0) {
+		return '+' + formattedValue.toFixed(1) + '%';
+	}
+	return formattedValue.toFixed(1) + '%';
 }
 
 export default function Summary(props) {
-  const classes = useStyles();
-  const today = () => {
-    let today = new Date();
-    let dd = String(today.getDate()); // .padStart(2, '0');
-    let mm = String(today.getMonth() + 1); // .padStart(2, '0'); //January is 0!
-    let yyyy = today.getFullYear();
-    let hours = today.getHours();
-    let minutes = today.getMinutes();
-    var ampm = hours >= 12 ? 'pm' : 'am';
-    hours = hours % 12;
-    hours = hours ? hours : 12; // the hour '0' should be '12'
-    minutes = minutes < 10 ? '0'+minutes : minutes
-    return mm + '/' + dd + '/' + yyyy + ', ' + hours + ':' + minutes + ampm;
-  };
+	const classes = useStyles();
+	const today = () => {
+		let today = new Date();
+		let dd = String(today.getDate()); // .padStart(2, '0');
+		let mm = String(today.getMonth() + 1); // .padStart(2, '0'); //January is 0!
+		let yyyy = today.getFullYear();
+		let hours = today.getHours();
+		let minutes = today.getMinutes();
+		var ampm = hours >= 12 ? 'pm' : 'am';
+		hours = hours % 12;
+		hours = hours ? hours : 12; // the hour '0' should be '12'
+		minutes = minutes < 10 ? '0' + minutes : minutes
+		return mm + '/' + dd + '/' + yyyy + ', ' + hours + ':' + minutes + ampm;
+	};
 
-  const ytd = formatPercentage(props.yearToDatePerformance['Total Percent Change']);
-  const daily = formatPercentage(props.yearToDatePerformance['Daily Percent Change']);
+	const ytd = formatPercentage(props.yearToDatePerformance['Total Percent Change']);
+	const daily = formatPercentage(props.yearToDatePerformance['Daily Percent Change']);
 
-  return (
-    <React.Fragment>
-      <Title>Year to Date</Title>
-      <Typography component="p" variant="h5" className={ytd.indexOf('-') > -1 ? classes.negative : classes.positive}>
-        {ytd}
-      </Typography>
-      <Title>Daily Change</Title>
-      <Typography component="p" variant="h5" className={daily.indexOf('-') > -1 ? classes.negative : classes.positive}>
-        {daily}
-      </Typography>
-      <Typography color="textSecondary" className={classes.neutral}>
-        as of {today()}
-      </Typography>
-      {/* <div>
+	return (
+		<React.Fragment>
+			<Title>Year to Date</Title>
+			<Typography component="p" variant="h5" className={ytd.indexOf('-') > -1 ? classes.negative : classes.positive}>
+				{ytd}
+			</Typography>
+			<Title>Daily Change</Title>
+			<Typography component="p" variant="h5" className={daily.indexOf('-') > -1 ? classes.negative : classes.positive}>
+				{daily}
+			</Typography>
+			<Typography color="textSecondary" className={classes.neutral}>
+				as of {today()}
+			</Typography>
+			{/* <div>
         <Link color="primary" href="#" onClick={preventDefault}>
           View balance
         </Link>
       </div> */}
-    </React.Fragment>
-  );
+		</React.Fragment>
+	);
 }
